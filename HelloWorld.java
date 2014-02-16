6a9a127c572103cfca01b250e2c822f1a68630e2
unknown <apkarthick@gmail.com>
2014-02-16 23:06:03 +0530

    My second Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@
 public class HelloWorld{
 	public static void main(String args[]){
 		System.out.println("My First git");
+		System.out.println("Ok done my first commit already. Ready for new Commit");
 	}
 }
